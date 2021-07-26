30a12548e8c16ebb6ffb5c302161ee34058b8570
mohanram <mohanram.shanmugam96@gmail.com>
2021-07-26 19:51:22 +0530

    link to consultant page

--- src/HomePageComponents/Header.js
+++ src/HomePageComponents/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "../Assets/images/logo.png";
 import Back from "../Assets/images/ic-back.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -12,12 +13,21 @@ const Header = () => {
               <img src={Back} width="15" alt="BACK" />
             </aside>
             <aside className="col-lg-3 col-md-3 col-10 p-0 sc-logo">
-              <img src={Logo} className="img-fluid" alt="Scout&amp;Cellar-WineConsultant" />
+              <img
+                src={Logo}
+                className="img-fluid"
+                alt="Scout&amp;Cellar-WineConsultant"
+              />
             </aside>
             <aside className="col-lg-8 col-md-8 col-6 p-0 pt-2 d-md-block">
-              <a className="btn btn-primary" href="javascript:;" role="button" title="GET STARTED">
+              <Link
+                className="btn btn-primary"
+                to="/consultant"
+                role="button"
+                title="GET STARTED"
+              >
                 GET STARTED<i className="ic-arrow">&nbsp;</i>
-              </a>
+              </Link>
             </aside>
           </section>
         </section>
