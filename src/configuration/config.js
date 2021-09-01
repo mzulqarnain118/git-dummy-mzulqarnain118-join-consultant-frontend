dfbd14f9f1d7d29748dba51d6490e05d8ea8213e
“ashwinbala” <ashwin.bala@influx.co.in>
2021-09-01 20:56:00 +0530

    committing base url

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -2,7 +2,7 @@
 
 //reCaptcha site key
 export const site_key = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
-
+var baseUrl = "https://apidevscoutncellar.influx.co.in";
 /**
  * Determine the mobile operating system.
  * This function returns one of 'iOS', 'Android', or 'unknown'.
@@ -25,11 +25,10 @@ export const algoliaURL =
   "https://565g60m4tz-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(3.35.1)%3B%20Browser%20(lite)%3B%20react%20(16.9.0)%3B%20react-instantsearch%20(5.7.0)%3B%20JS%20Helper%20(2.28.1)&x-algolia-application-id=565G60M4TZ&x-algolia-api-key=90b94f7411220a5d6b88644b81a474c9";
 
 //algoliya get user data
-export const getWorkingWithURL =
-  "https://apidevscoutncellar.influx.co.in/api/v1/users/consultantReferral";
+export const getWorkingWithURL = baseUrl + "/api/v1/users/consultantReferral";
 
 const constant = {
-  baseUrl: "https://apidevscoutncellar.influx.co.in",
+  baseUrl: baseUrl,
   username: "appuser@injin.com",
   password: "Test@123",
 };
