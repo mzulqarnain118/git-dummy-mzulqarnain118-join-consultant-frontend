b79b321bcfe05c7d64f92043a5fe582852eb535a
Ashwin.bala <Ashwin.bala@influx.co.in>
2021-09-09 07:06:19 +0530

    updated android link

--- src/CreateConsultantComponents/PaymentConfirmation/PaymentConfirmation.js
+++ src/CreateConsultantComponents/PaymentConfirmation/PaymentConfirmation.js
@@ -361,7 +361,7 @@ class PaymentConfirmation extends React.Component {
                       />
                     </a>
                     <a
-                      href="https://play.google.com/store/apps/details/?id=co.vine.android"
+                      href="https://play.google.com/store/apps/details?id=com.sandc.vine"
                       target={"_blank"}
                     >
                       <img
