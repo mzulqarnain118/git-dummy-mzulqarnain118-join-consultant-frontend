1908e06cb5f990997509dfac5075508d705ca535
Chris Carrillo <chriscarrillo@outlook.com>
2023-05-17 15:20:14 -0500

    #24 Promo discount

--- src/CreateConsultantComponents/Home/Home.js
+++ src/CreateConsultantComponents/Home/Home.js
@@ -799,14 +799,14 @@ class Home extends React.Component {
         .then((response) => {
           try {
             purchaseKitDetails["subtotal"] =
-              response.data.OrderLines[0].Subtotal;
+              response.data.Subtotal;
             purchaseKitDetails["shipping"] =
               response.data.OrderLines[0].ShippingTax;
             purchaseKitDetails["salestax"] =
               response.data.OrderLines[0].ItemTax;
             purchaseKitDetails["discount"] =
-              response.data.OrderLines[0].Discounts;
-            purchaseKitDetails["total"] = response.data.OrderLines[0].LineTotal;
+              response.data.OrderLines[0].Discounts || response.data.OrderLines[1]?.Discounts || 0;
+            purchaseKitDetails["total"] = response.data.SubtotalAfterSavings;
             purchaseKitDetails["discountDescription"] =
               response.data.DiscountTotals.length > 0
                 ? response.data.DiscountTotals[0].TotalDescription
